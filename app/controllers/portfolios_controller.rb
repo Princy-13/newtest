580b83fd6a98e263b1ba9f03b3f25f0982c5bb31
princy mary <maryprincee13@gmail.com>
2021-06-22 12:32:58 +0530

    implemented delete functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -44,5 +44,17 @@ class PortfoliosController < ApplicationController
     def show
         @portfolio_item = Portfolio.find(params[:id])
     end 
+    
+    def destroy
+      @portfolio_item =Portfolio.find(params[:id])
+
+      @portfolio_item.destroy
+
+     respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "portfolio_item was successfully destroyed." }
+    
+     end
+
+    end
 
 end
