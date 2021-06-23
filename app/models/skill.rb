f0605ed28fe9a1907548a211927e9a50341519f4
princy mary <maryprincee13@gmail.com>
2021-06-23 16:31:06 +0530

    implemented technologies model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,8 +1,5 @@
 class Skill < ApplicationRecord
     validates_presence_of :title,:percent_utilized
 
-    after_initialize:set_defaults
-    def set_defaults
-        self.main_image ||= Placeholder.image_generator(height:'250',width:'250')
-    end
+    
 end
